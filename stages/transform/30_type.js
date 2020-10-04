02a1c889a3944ddec4e98af38b9d4ebe04bb69c9
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-10-04 21:21:00 +0200

    Bump  lastejobb  ^2.17.6  →  ^3.8.2

--- stages/transform/30_type.js
+++ stages/transform/30_type.js
@@ -1,4 +1,4 @@
 const { io } = require("lastejobb");
 
-let data = io.lesDatafil("maritim-grense-ubehandlet/type");
+let data = io.lesTempJson("maritim-grense-ubehandlet/type");
 io.skrivBuildfil("type", data);
